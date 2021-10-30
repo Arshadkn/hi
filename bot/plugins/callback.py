fb16ada1e0903977e16f189e0c57111384168278
Arshadkn <93204996+Arshadkn@users.noreply.github.com>
2021-10-30 20:14:55 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -142,7 +142,7 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
     
-    text=f"<i>Check spelling 🎖️Found</i> <code>{leng}</code> <i>Results For Your Result🎖️:</i> <code>{query}</code>"
+    text=f"<i>Check link @naichannel 🎖️Found</i> <code>{leng}</code> <i> For Your Result🎖️:</i> <code>{query}</code>"
         
     try:
         await update.message.edit(
